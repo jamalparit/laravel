e9886af05a7ae7dad315adfdb756d84e052ad6de
jamalparit <jamalparit@outlook.com>
2014-07-15 04:06:19 +0400

    Error ms47 baris 5 -dah sampai ms 48

--- public_html/app/config/database.php
+++ public_html/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'myjkns',
+			'username'  => 'root',
+			'password'  => 'htar6070',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
